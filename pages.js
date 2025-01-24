49ccfcdc3e0bd6cd53ecfdee6c0e69bf66e900c1
mhd-radin <radinpaliyalill@gmail.com>
2025-01-24 09:38:58 +0530

    update pages.js

--- pages.js
+++ pages.js
@@ -1,8 +1,12 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+
 const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.get("/:fileID", function (req, res) {
+router.get("/:fileID", function(req, res) {
   res.sendFile(path.join(__dirname, "/localStore/" + req.params.fileID));
 });
 
