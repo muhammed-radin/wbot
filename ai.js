c67fe450e576123812f62dca7d8696ff790599ff
mhd-radin <radinpaliyalill@gmail.com>
2025-01-24 08:26:31 +0530

    update ai.js

--- ai.js
+++ ai.js
@@ -1,22 +1,32 @@
 const { Client } = require("@gradio/client");
 
-const client = await Client.connect("Qwen/Qwen2.5-Coder-demo");
+let client = null;
 let history = [["Hello!", 'How can help you?'], ['Which year started Bushido?', 'Since 2016']]
 
+
+(async function(qes) {
+  client = await Client.connect("Qwen/Qwen2.5-Coder-demo");
+})();
+
+
 async function runAi(qes) {
-  const result = await client.predict("/model_chat", {
-    query: qes,
-    history: history,
-    system: "An Expert martial art coach who can assist others. name is Bushido. Bushido is boxer club and a institution located on Asia, India, Kerela state, Malappuram, at Perithanalamena",
-    radio: "0.5B",
-  });
+  if (client) {
+    const result = await client.predict("/model_chat", {
+      query: qes,
+      history: history,
+      system: "An Expert martial art coach who can assist others. name is Bushido. Bushido is boxer club and a institution located on Asia, India, Kerela state, Malappuram, at Perithanalamena",
+      radio: "0.5B",
+    });
 
 
-  let root = result.data[1]
-  let msg = root[root.length - 1];
-  history = root;
-  console.log(msg)
-  return msg[1];
+    let root = result.data[1]
+    let msg = root[root.length - 1];
+    history = root;
+    console.log(msg)
+    return msg[1];
+  } else {
+
+  }
 }
 
 function handleErr(err) {
